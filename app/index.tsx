458d37be62ec0721004b189d403d795696327da0
DilshanSandhu <dilshansingh.sandhu@edu.sait.ca>
2026-02-05 18:56:27 -0700

    updated the layout which was missing some elements and added comments for others

--- app/index.tsx
+++ app/index.tsx
@@ -3,26 +3,42 @@ import { Text, View, Image, StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View style = {styles.container}>
-      <Text style={styles.header}>Posts</Text>
+      <View style={styles.topbar}>
+        <Text style={styles.arrow}>{"<"}</Text>
+        <Text style={styles.header}>Posts</Text>
+      </View>
+
+      <View style={styles.userRow}>
+          {/* put the user profile picture name and menu three dota here */}
+      </View>
+    
       <Image
       // Absolute Url: "https://www.istockphoto.com/photo/scenic-aerial-view-of-the-mountain-landscape-with-a-forest-and-the-crystal-blue-gm1458782106-493292590"
       source={require("../assets/images/a.jpg")}
       style={styles.image}/>
       
-      <Text style={styles.likes}>Liked by 
-        <Text style={styles.username}> paisley.print.48</Text> and 
-        <Text style={styles.username}> 7 others</Text>
-      </Text>
+      {/* put like share and comment icons here */}
 
-      <Text style={styles.caption}>
-        <Text style={styles.username}>frenchie_fry39 </Text>
+      <View style={styles.TextSection}> 
+        <Text style={styles.likes}>Liked by 
+        <Text style={styles.bold}> paisley.print.48</Text> and 
+        <Text style={styles.bold}> 7 others</Text>
+        </Text>
+
+        <Text style={styles.caption}>
+        <Text style={styles.bold}>frenchie_fry39 </Text>
         Fresh shot on a sunny day</Text>
 
-        <Text style={styles.comment}>View all 12 comments</Text>
-        <Text style={styles.comments}>
-          <Text style={styles.username2}>lil_wyatt838</Text> Awesome tones</Text>
-        <Text style={styles.comments}><Text style={styles.username2}>pia.in.a.pod</Text> Love it!</Text>
+        <Text style={styles.viewComments}>View all 12 comments</Text>
+        <Text style={styles.comment}>
+        <Text style={styles.bold2}>lil_wyatt838</Text> Awesome tones</Text>
+        <Text style={styles.comment}><Text style={styles.bold2}>pia.in.a.pod</Text> Love it!</Text>
+
+        <Text style={styles.time}>1 day ago</Text>
+        </View>
     </View>
+
+    // please add the alert button here at the end
   );
 }
 
@@ -52,21 +68,31 @@ const styles = StyleSheet.create({
     fontSize: 14,
     marginBottom: 8,
   },
-  username: { 
+  bold: {
     fontWeight: "bold",
     fontSize: 14,
   },
-  username2: {
+  bold2 : {
     fontWeight: "bold",
-    fontSize: 10,
-  },
-  comments: {
-    fontSize: 10,
-    marginBottom: 2,
+    fontSize: 12,
   },
   comment: {
-    fontSize: 10,
+    fontSize: 12,
+    marginBottom: 2,
+  },
+  viewComments: {
+    fontSize: 12,
     color: "gray",
     marginBottom: 4,
+  },
+  time: {
+  },
+  topbar: {
+  },
+  arrow: {
+  },
+  userRow: {
+  },
+  TextSection: {
   },
 });
